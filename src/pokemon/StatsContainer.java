69e8b55a71426d8b7ff8106c7d7f9d733b117c56
TheJuanderer <brentmichaelmendozaup@gmail.com>
2025-12-19 01:32:18 +0700

    implemented the stats modifier

--- src/pokemon/StatsContainer.java
+++ src/pokemon/StatsContainer.java
@@ -8,6 +8,13 @@ public class StatsContainer {
 	private int specAtk = 0;
 	private int specDef = 0;
 	
+	// modifiers for non-HP stats
+    private int physAtkMod = 0;
+    private int physDefMod = 0;
+    private int speedMod = 0;
+    private int specAtkMod = 0;
+    private int specDefMod = 0;
+	
 	public StatsContainer(int[] statsArray) {
 		hp = statsArray[0];
 		physAtk = statsArray[1];
@@ -35,24 +42,52 @@ public class StatsContainer {
 	
 	public int getStat(String statName) {
 		switch (statName) {
-			case "hp":
-				return hp;
-			case "physAtk":
-				return physAtk;
-			case "physDef":
-				return physDef;
-			case "speed":
-				return speed;
-			case "specAtk":
-				return specAtk;
-			case "specDef":
-				return specDef;
-			
-			default:
-				throw new RuntimeException("Invalid stat name.");
+		case "hp":
+			return this.hp;
+		case "physAtk":
+			return this.physAtk;
+		case "physDef":
+			return this.physDef;
+		case "speed":
+			return this.speed;
+		case "specAtk":
+			return this.specAtk;
+		case "specDef":
+			return this.specDef;
+		
+		default:
+			throw new RuntimeException("Invalid stat name.");
+
+
 		}
 	}
 	
+	
+	//used for moves that increases atk for example
+	public void applyModStat(String statName, int modifier) {
+		switch (statName) {
+		case "physAtk":
+			this.adjustAtkMod(modifier); //sets the new modifier
+			this.physAtk = this.applyModifier(physAtk, this.limitMod(modifier));//calculate the modified stat
+		case "physDef":
+			this.adjustDefMod(modifier);
+			this.physDef = applyModifier(physDef, this.limitMod(modifier));
+		case "speed":
+			this.adjustSpeedMod(modifier);
+			this.speed = applyModifier(speed, this.limitMod(modifier));
+		case "specAtk":
+			this.adjustSpAtkMod(modifier);
+			this.specAtk = applyModifier(specAtk, this.limitMod(modifier));
+		case "specDef":
+			this.adjustSpDefMod(modifier);
+			this.specDef = applyModifier(specDef, this.limitMod(modifier));
+		
+		default:
+			throw new RuntimeException("Invalid stat name.");
+		}
+	}
+	
+	//for stats modifiers
 	public void setStat(String statName, int value) {
 		switch (statName) {
 			case "hp":
@@ -79,9 +114,11 @@ public class StatsContainer {
 		}
 	}
 	
+	//this is the Pokemon's mutable stats, the original is at PokemonSpecies
 	public StatsContainer clone() {
 		StatsContainer clonedStats = new StatsContainer(new int[6]);
 		
+		//set it to normal values first, we can always use the setter for the modifiers
 		clonedStats.setHp(hp);
 		clonedStats.setAtk(physAtk);
 		clonedStats.setDef(physDef);
@@ -92,11 +129,48 @@ public class StatsContainer {
 		return clonedStats;
 	}
 
-
+	// Apply modifier formula
+    private int applyModifier(int stat, int mod) {
+        if (mod >= 0) {
+            return (int)Math.round(((2.0 + mod)/2.0) * stat);
+        } else {
+            return (int)Math.round((2.0 / (2.0 + mod)) * stat);
+        }
+    }
+    
+    
+    //this func is to limit the modifiers to -4 to +4
+    private int limitMod(int mod) {
+        return Math.max(-4, Math.min(4, mod));
+    }
+    
+    
 	@Override
 	public String toString() {
 		return "HP = " + hp + " / ATK = " + physAtk + " / DEF = " + physDef + " / SPE = " + speed
 				+ " / SPA = " + specAtk + " / SPD = " + specDef;
 	}
 	
+	//setters for Stats Modifiers
+	//this will either increment or decrement based on the sign of the parameter
+    public void adjustAtkMod(int mod) { physAtkMod += this.limitMod(mod); }
+    public void adjustDefMod(int mod) { physDefMod += this.limitMod(mod); }
+    public void adjustSpeedMod(int mod) { speedMod += this.limitMod(mod); }
+    public void adjustSpAtkMod(int mod) { specAtkMod += this.limitMod(mod); }
+    public void adjustSpDefMod(int mod) { specDefMod += this.limitMod(mod); }
+    
+    //getters for stats modifiers
+    public int getAtkMod() { return physAtkMod; }
+    public int getDefMod() { return physDefMod; }
+    public int getSpeedMod() { return speedMod; }
+    public int getSpAtkMod() { return specAtkMod; }
+    public int getSpDefMod() { return specDefMod; }
+
+    
+    
+    
+    
+    
+    
+    
 }
