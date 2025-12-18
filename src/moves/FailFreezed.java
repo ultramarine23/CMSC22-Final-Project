adc5481e9c25a170626af7f90ff7ad77d43224ec
TheJuanderer <brentmichaelmendozaup@gmail.com>
2025-12-18 15:26:41 +0700

    added failed moves for freezed and sleeped pokemon

---
+++ src/moves/FailFreezed.java
@@ -0,0 +1,29 @@
+package moves;
+import java.util.EnumSet;
+
+import main.BattleContext;
+import main.Globals.MoveCategory;
+import main.Globals.MoveFlags;
+import main.Globals.Types;
+import pokemon.Pokemon;
+
+public class FailFreezed extends Move {
+
+	public FailFreezed() {
+		super("[Freezed]", 0, 100, MoveCategory.STATUS, Types.NONE, 0, 100, "", EnumSet.noneOf(MoveFlags.class));
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void beforeExecution(Pokemon user, Pokemon target, BattleContext ctx) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void afterExecution(Pokemon user, Pokemon target, BattleContext ctx) {
+		System.out.println("Does not move");
+
+	}
+
+}
---
+++ src/moves/FailSleep.java
@@ -0,0 +1,30 @@
+package moves;
+
+import java.util.EnumSet;
+
+import main.BattleContext;
+import main.Globals.MoveCategory;
+import main.Globals.MoveFlags;
+import main.Globals.Types;
+import pokemon.Pokemon;
+
+public class FailSleep extends Move {
+
+	public FailSleep() {
+		super("[Sleeps]", 0, 100, MoveCategory.STATUS, Types.NONE, 0, 100, "", EnumSet.noneOf(MoveFlags.class));
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void beforeExecution(Pokemon user, Pokemon target, BattleContext ctx) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void afterExecution(Pokemon user, Pokemon target, BattleContext ctx) {
+		System.out.println(user.getPokemonSpecies().getName() + "is fast asleep");
+
+	}
+
+}
