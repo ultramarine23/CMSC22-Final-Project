37c41302d36f938525e2f698f6a0d67d7dd49df7
pojomo <cudianot@gmail.com>
2025-12-20 03:27:42 +0800

    ADDED META MOVES

--- src/moves/Explosion.java
+++ src/moves/Explosion.java
@@ -1,39 +1,41 @@
 package moves;
 
+
+
 import java.util.EnumSet;
+
 import main.BattleContext;
 import main.Globals.MoveCategory;
 import main.Globals.MoveFlags;
 import main.Globals.Types;
 import pokemon.Pokemon;
-import main.TurnHistory;
 
+public class Explosion extends Move {
 
-public class DrainPunch extends Move {
-	public DrainPunch() {
+	public Explosion() {
 		super(
-				"Drain Punch", 
-				75, 
+				"Explosiom", 
+				250, 
 				100, 
 				MoveCategory.PHYSICAL, 
-				Types.FIGHTING, 
+				Types.NORMAL, 
 				0, 
-				16, 
-				"User recovers 50% of the damage dealt.", 
-				EnumSet.of(MoveFlags.HEALING_MOVE, MoveFlags.CONTACT_MOVE));
+				5, 
+				"Self-destruct inflicts damage and causes the user to faint.", 
+				EnumSet.noneOf(MoveFlags.class)
+			);
+		//Does not make contact
 	}
 
 	@Override
 	public void beforeExecution(Pokemon user, Pokemon target, BattleContext ctx) {
-		// TODO Auto-generated method stub
+		// do nothing
 
 	}
 
 	@Override
 	public void afterExecution(Pokemon user, Pokemon target, BattleContext ctx) {
-		// gain back health
-		int damage = ctx.getHistory().damageTakenThisTurn(target);
-		user.healHealth(damage/2);
+		user.takeDamage(user.getCurrentStats().getHp());
 	}
 
 }
