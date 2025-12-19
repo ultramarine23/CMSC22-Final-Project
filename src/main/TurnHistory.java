c6a3f7ee89d71c7caeb483ad1ade0108666f9237
ultra <166610445+ultramarine23@users.noreply.github.com>
2025-12-19 12:12:34 +0800

    Priority now implemented properly

--- src/main/TurnHistory.java
+++ src/main/TurnHistory.java
@@ -1,5 +1,7 @@
 package main;
 
 public class TurnHistory {
-
+	/* turn history is a mutable data container set by the facade BattleContext throughout
+	 * the turn, keeping track of essential details that have happened this turn like  
+	 */
 }
