53cff8cf35f64f2a0f438743920937c1c0f3c0cd
TheJuanderer <brentmichaelmendozaup@gmail.com>
2025-12-20 22:03:43 +0700

    added JUnit Test

---
+++ src/main/JUnitTest.java
@@ -0,0 +1,26 @@
+package main;
+
+import pokemon.Pokemon;
+import pokemon.StatsManager;
+import pokemon.StatsContainer;
+
+
+public class JUnitTest {
+	//add all the objects that needs testing
+	StatsManager statsManager;
+	StatsContainer statsContainer;
+	
+
+	public JUnitTest() {
+		
+	}
+	
+	
+	void testStatsManager() {
+		
+	}
+	
+	void testStatsContainer() {
+		
+	}
+}
